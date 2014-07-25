e35bff711432b9863e4f4a1fefe050ae680823c5
lynnaevans <lynn@msa-inc.net>
2014-07-25 16:07:06 -0500

    First of five steps

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Jorge L. Reyes-Ortiz, Davide Anguita, Alessandro Ghio, Luca Oneto.
 Smartlab - Non Linear Complex Systems Laboratory
 DITEN - UniversitÃ degli Studi di Genova, Genoa I-16145, Italy.
 activityrecognition '@'smartlab.ws
-www.smartlab.ws
+www.smartlab.ws   
--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
-#
-# Citation here
-#
+#*******************************************************************************
+# Beginning Step 1. Merges the training and the test sets to create one data set
+#*******************************************************************************
 # Read in test data from the Working Directory
 #
 X_test <- read.table("X_test.txt", quote="\"")
@@ -47,6 +47,56 @@ final.train <- cbind(subject_train,combined.train)
 #
 combined.data <- rbind(final.train, final.test)
 dim(combined.data)
+# 10299 X 563
 #
 # Add the column titles
 #
+features <- read.table("features.txt", quote="\"", stringsAsFactors=FALSE)
+#
+#Get the column names from the second column of features
+theColumnNames <- as.data.frame(features[,2])
+# Create the first two column names
+firstTwoColumnNames<- as.data.frame(c("Subject", "Activity"))
+#Synchronize the column names of theColumnNames and firstTwoColumnNames
+names(theColumnNames)<- c("V1")
+names(firstTwoColumnNames)<-c("V1")
+# Add them together 
+new.column.names <- rbind(firstTwoColumnNames, theColumnNames)
+# Add the column names to the dataframe
+names(combined.data)<-new.column.names[,1]
+#*******************************************************************************
+# End of Step 1. Merges the training and the test sets to create one data set.
+#*******************************************************************************
+# Beginning of Step 2 . Extracts only the measurements on the mean and standard 
+#                       deviation for each measurement.
+#*******************************************************************************
+
+#*******************************************************************************
+# End of Step 2 . Extracts only the measurements on the mean and standard 
+#                 deviation for each measurement.
+#*******************************************************************************
+# Beginning of Step 3. Uses descriptive activity names to name the activities in
+#                      the data set        
+#*******************************************************************************
+
+#*******************************************************************************
+# End of Step 3. Uses descriptive activity names to name the activities in
+#                      the data set        
+#*******************************************************************************
+# Beginning of Step 4. Appropriately labels the data set with descriptive 
+#                      variable names. 
+#*******************************************************************************
+
+#*******************************************************************************
+# End of Step 4. Appropriately labels the data set with descriptive variable 
+#                names.            
+#*******************************************************************************
+# Beginning of Step 5. Creates a second, independent tidy data set with the 
+#                      average of each variable for each activity and each 
+#                      subject.
+#*******************************************************************************
+
+#*******************************************************************************
+# End of Step 5. Creates a second, independent tidy data set with the average of 
+#                each variable for each activity and each subject.
+#*******************************************************************************
