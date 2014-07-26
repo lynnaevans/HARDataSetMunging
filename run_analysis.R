a118f7417cac12fdf9f56b19c4c8bc7b298a896f
lynnaevans <lynn@msa-inc.net>
2014-07-26 17:16:40 -0500

    Final version

--- run_analysis.R
+++ run_analysis.R
@@ -8,6 +8,7 @@ X_test <- read.table("X_test.txt", quote="\"")
 # Make sure of the dimensions
 #
 dim(X_test)
+# [1] 2947  561
 #
 # Read in test activity vector (Values = 1 through 6)
 #
@@ -16,32 +17,43 @@ y_test <- read.table("y_test.txt", quote="\"")
 # Make sure of the dimensions
 #
 dim(y_test)
+# [1] 2947    1
 #
 # Now bind the 2947 X 1 vector of activity numbers to the 2947 X 561 data matrix
 #
 combined.test <- cbind(y_test, X_test)
 dim(combined.test)
+# [1] 2947  562
 #
 # Read in test subject vector (Values = 1 through 30)
 #
 subject_test <- read.table("subject_test.txt", quote="\"")
 dim(subject_test)
+#[1] 2947    1
 #
 # Now bind the 2947 X 1 vector of subjext numbers to the 2947 X 562 data matrix
 #
 final.test <- cbind(subject_test,combined.test)
+dim(final.test)
+#[1] 2947  563
 #
 # Repeat the above steps for the training data
 #
 X_train <- read.table("X_train.txt", quote="\"")
 dim(X_train)
+#[1] 7352  561
 y_train <- read.table("y_train.txt", quote="\"")
 dim(y_train)
+#[1] 7352  561
 combined.train <- cbind(y_train, X_train)
 dim(combined.train)
+#[1] 7352  562
 subject_train <- read.table("subject_train.txt", quote="\"")
 dim(subject_train)
+#[1] 7352    1
 final.train <- cbind(subject_train,combined.train)
+dim(final.train)
+#[1] 7352  563
 #
 # Combine the two dataframes
 #
